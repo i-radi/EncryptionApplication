cb654d47c50889bf135d31a58ec634149ba2e28a
i-radi <hema.alprof5970@gmail.com>
2020-07-19 23:05:04 +0200

    first commit

---
+++ ConsoleApplication/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EncryptionV1;
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter Key: ");
+            var key = Console.ReadLine();
+            Console.WriteLine("Enter Message: ");
+            var message = Console.ReadLine();
+
+            try
+            {
+                var obj = new Encryption(key);
+                Console.WriteLine(obj.getCipher(message));
+            }
+            catch
+            {
+                Console.WriteLine("Error etl3 mn hna");
+            }
+
+        }
+    }
+}
